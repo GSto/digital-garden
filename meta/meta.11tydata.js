0a449508dd9adeaa3b54dcebacd4d66143a283f4
Glenn Stovall <glennstovall@gmail.com>
2022-04-26 09:24:58 -0400

    css fixes, style fixes, missing link fixes.

--- meta/meta.11tydata.js
+++ meta/meta.11tydata.js
@@ -4,38 +4,32 @@ const { getOutboundLinks } = require("../lib/notes");
 const getBrokenLinks = (notes) => {
   // array of all possible note URLs
   const noteKey = notes.map((note) => note.data.title);
-  const brokenLinks = {}
+  const brokenLinks = {};
   for (const note of notes) {
     const outboundLinks = getOutboundLinks(note);
     for (link of outboundLinks) {
       // if the link does not exist in the key
-      if (!noteKey.some(key => caselessCompare(key, link))) {
+      if (!noteKey.some((key) => caselessCompare(key, link))) {
         // if it exists, add the link to the array
-        if(brokenLinks[link]) {
-
-          brokenLinks[link] = [...new Set(
-            [
-              ...brokenLinks[link],
-              note
-            ]
-          )]
+        if (brokenLinks[link]) {
+          brokenLinks[link] = [...new Set([...brokenLinks[link], note])];
         } else {
-          brokenLinks[link] = [note]
+          brokenLinks[link] = [note];
         }
       }
     }
   }
 
   return Object.entries(brokenLinks).sort((a, b) => {
-    if(a[1].length > b[1].length) {
-      return -1
-    } else if(a[1].length < b[1].length) {
-      return 1
+    if (a[1].length > b[1].length) {
+      return -1;
+    } else if (a[1].length < b[1].length) {
+      return 1;
     } else {
-      return a[0].toLowerCase() > b[0].toLowerCase() ? 1 : -1
+      return a[0].toLowerCase() > b[0].toLowerCase() ? 1 : -1;
     }
   });
-}
+};
 
 module.exports = {
   eleventyComputed: {
--- meta/missing.md
+++ meta/missing.md
@@ -1,15 +1,18 @@
 ---
 layout: note
 ---
+
 # Missing Links ({{brokenLinks | size}})
 
 A list of broken internal links. can serve as inspiration for parts of the graph that may call for further information.
-Some links may be misspelled or mis-termed versions of existing links. These should be fixed. 
+Some links may be misspelled or mis-termed versions of existing links. These should be fixed.
 
-missing links with multiple instances are a sign they should be fixed. 
+missing links with multiple instances are a sign they should be fixed.
 
 some of these links will never exist, but that doesn't mean you should remove them. missing links are a feature not a bug.
 
 {% for link in brokenLinks %}
-- {{ link[0] }} ({% for source in link[1] %}[{{source.data.title}}]({{source.data.url}}), {% endfor %})
-{% endfor %}
+
+- {{ link[0] }} {% for source in link[1] %}
+  - [{{source.data.title}}]({{source.url | replace: " ", "%20"}}){% endfor %}
+    {% endfor %}
